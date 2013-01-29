5ca619bd6ea9b170e4e4a3a2d489ca0a7641ba6f
Nicholas Moline <nick.moline@justia.com>
2013-01-28 17:21:16 -0800

    move to subfolder

---
+++ sample/config.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * Copyright (C) 2013 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// TODO: You must configure these fields for the starter project to function.
+// Visit https://developers.google.com/glass/getting-started to learn more
+$api_client_id = "862810043441-5ltusk02nu0ei5mdkdvpv6qi0dcdsvup.apps.googleusercontent.com";
+$api_client_secret = "IhJvx_BR3iNt3fbRtnPqIfto";
+$api_simple_key = "AIzaSyD5tkma1YOOcIwANNG5QmuaNnk9ZG_WOGw";
+
+$service_base_url = "http://glassteam2013.ocdhost.com/";
