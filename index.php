017a6b32b7829a80252edcdc69350ac3cda7ca40
Nicholas Moline <nick.moline@justia.com>
2013-01-29 14:11:33 -0800

    url paths should be /

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if (array_key_exists('token', $_SESSION)) {
   $token = login_user();
 }
 
-echo "<html><head><title>Glass Eye</title></head><body bgcolor=\"black\" text=\"white\"><p align=\"center\"><img src=\"images\header.png\"></p>";
+echo "<html><head><title>Glass Eye</title></head><body bgcolor=\"black\" text=\"white\"><p align=\"center\"><img src=\"images/header.png\"></p>";
 
 if ($token) {
 
@@ -20,7 +20,7 @@ if ($token) {
 echo "
 <form method=\"post\">
   <input type=\"hidden\" name=\"operation\" value=\"installApp\"/>
-  <p align=\"center\"><input type=\"image\" src=\"images\button.png\" name=\"submit\" border=\"0\"></p>
+  <p align=\"center\"><input type=\"image\" src=\"images/button.png\" name=\"submit\" border=\"0\"></p>
 </form>";
 }
 
